4081cc90b262b849423807591f76a5da6d3030a2
SnehaDemanna <99233550+SnehaDemanna@users.noreply.github.com>
2023-01-12 18:42:06 +0530

    Add files via upload

---
+++ BigBasket/src/test/java/bigbasket/runnerClass.java
@@ -0,0 +1,19 @@
+package bigbasket;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="src/test/resources/featureFiles/BigBasket.feature",
+		tags ="@regressionTest"
+		//tags = "@smokeTest"
+
+		)		
+
+public class runnerClass {
+
+}
+
